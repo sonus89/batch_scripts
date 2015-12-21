22bd5d55730b1cbd6c6afe894c30dffb612a4217
sonus89 <gal.mateo@gmail.com>
2015-12-21 03:20:44 +0100

    gcc.bat

--- gcc.sh
+++ gcc.sh
@@ -1,17 +1,3 @@
-# There is no warranty. Nobody can guarantee that this will work.                   #
-# This script compiles a C source file for Atmel AVR Atmega128 MCU with AVR-GCC     #
-# and creates *.elf *.o and *.hex files as output                                   #
-#                                                                                   #
-# Written by Matthew Gaal                                                           #
-# 2015 - Oct - 24                                                                   #
-#                                                                                   #
-#####################################################################################
-
-# EXAMPLE: "sh gcc.sh something" compiles something.c and creates object, hex and elf
-
-#!/bin/bash
-file_name=$1
-
-avr-gcc -g -Os -mmcu=atmega128 -c $file_name.c && 
-avr-gcc -g -mmcu=atmega128 -o $file_name.elf $file_name.o && 
-avr-objcopy -j .text -j .data -O ihex $file_name.elf $file_name.hex
+set mypath=%cd%
+set file_name=%1
+C:\cygwin64\bin\gcc.exe %mypath%\%file_name%.c -o %mypath%\%file_name%.exe
